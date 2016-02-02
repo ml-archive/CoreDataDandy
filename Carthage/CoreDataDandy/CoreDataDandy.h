295ab4a00abe0f188001d45300cdb165e0188b4b
Noah Blake <noah@fuzzproductions.com>
2016-02-02 18:20:44 -0500

    Beginning work on Carthage support

---
+++ Carthage/CoreDataDandy/CoreDataDandy.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataDandy.h
+//  CoreDataDandy
+//
+//  Created by Noah Blake on 2/2/16.
+//  Copyright © 2016 Fuzz Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDataDandy.
+FOUNDATION_EXPORT double CoreDataDandyVersionNumber;
+
+//! Project version string for CoreDataDandy.
+FOUNDATION_EXPORT const unsigned char CoreDataDandyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataDandy/PublicHeader.h>
+
+
